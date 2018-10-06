b19d8a4ff339b2597cfe6b297d4ae620586d20cc
Joar <joar.andersson@elev.ga.lbs.se>
2018-10-06 14:10:19 +0200

    wack moving and shooting ship goes fast yeah boi

---
+++ Assets/Scripts/ProjectileScripts/PlayerShoot.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerShoot : ProjectileBase
+ {
+
+    private bool shooting;
+    private float nextShot;
+    public Transform projectileSpawn;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            shooting = true;
+        }
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            shooting = false;
+        }
+    }
+	
+	void FixedUpdate () 
+    {
+        if (nextShot >= 0)
+        {
+            nextShot -= Time.deltaTime;
+        }
+        if (shooting && nextShot <= 0)
+        {
+            Instantiate(projectile, projectileSpawn.position, projectileSpawn.rotation);
+            nextShot = firerate;
+        }
+	}
+}
---
+++ Assets/Scripts/ProjectileScripts/ProjectileBase.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjectileBase : MonoBehaviour
+{
+
+    public GameObject projectile;
+
+    [Range(1f,0.1f)]
+    public float firerate;
+}
